5b5b73dde6e17a4f8802ad72bde98ee033a97610
Luis Mayta <slovacus@gmail.com>
2014-07-18 11:36:39 -0500

    added service_manager factories

--- src/config/autoload/global.php
+++ src/config/autoload/global.php
@@ -18,4 +18,15 @@ return array(
         'namespace' => 'ZfSimpleMigrations\Migrations',
         'show_log' => true
     ),
-);
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
+            'navigation' => 'Zend\Navigation\Service\DefaultNavigationFactory',
+            ),
+        ),
+        'view_manager' => array(
+            'base_path' => "http://api.oauth.ec",
+            'display_not_found_reason' => true,
+            'display_exceptions' => true,
+        ),
+    );
